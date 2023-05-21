9aca662fd39e1ef83dbfa1d0962786250faa9831
Carlos ALvarez <carlosmail1978@gmail.com>
2023-05-21 11:27:22 +0200

    Add more user information fields

--- src/database/models/User.ts
+++ src/database/models/User.ts
@@ -9,6 +9,15 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+  name: {
+    type: String,
+  },
+  picture: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
 });
 
 const User = model("User", UserSchema, "users");
