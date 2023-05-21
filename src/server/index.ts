08d31c0dd71a48ea66140d40881d680ecc42c5d8
Carlos ALvarez <carlosmail1978@gmail.com>
2023-05-21 21:46:08 +0200

    Add cors config

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,15 @@
 import express from "express";
+import cors from "cors";
 import morgan from "morgan";
 import userRouter from "./routers/users/userRouter.js";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+  })
+);
 
 app.use(morgan("dev"));
 
